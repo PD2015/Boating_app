e76dca073e4a91268bfecf08625464770e2a7a90
Liz Stevenson <lizstevenson4@gmail.com>
2015-09-19 15:12:21 +0100

    Installed devise gem, conformed to 4 requirements, generated devise views, a user table and migrated db. Created link on home page to new usr reg.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # required for heroku/devise
+  # Note need to set this to your actual development url when it's in production.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
